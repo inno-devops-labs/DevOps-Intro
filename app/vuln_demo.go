053da8ce5c4c6883d210b1f9332912a010446669
Tivdzualubem <tivdzualubem@gmail.com>
2026-07-01 18:41:44 +0300

    lab9: remove vulnerable dependency and restore green gate

--- app/vuln_demo.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "golang.org/x/text/language"
-
-// runVulnerableDependencyDemo deliberately calls a vulnerable dependency
-// version for the Lab 9 govulncheck red/green CI demonstration.
-func runVulnerableDependencyDemo() {
-	_, _ = language.Parse("en-US")
-}
