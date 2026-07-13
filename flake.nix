ef427153043a0dba307459f3ed861f9f5e701fc9
Ilia Siaglov <ilya.syaglovv@gmail.com>
2026-07-13 19:18:06 +0800

    Feature/lab11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "QuickNotes — reproducible build with Nix";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs }:
     let
