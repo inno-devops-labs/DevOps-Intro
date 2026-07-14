dfbfc659a94f2209fe4e6dee7feb3ee224192d5f
MostafaKhaled2017 <m.kira@innopolis.university>
2026-07-14 18:49:18 +0300

    Solving task 2

--- flake.nix
+++ flake.nix
@@ -33,10 +33,50 @@
           "-w"
         ];
       };
+
+      docker = pkgs.dockerTools.buildImage {
+        name = "quicknotes";
+        tag = "lab11";
+
+        copyToRoot = quicknotes;
+
+        extraCommands = ''
+          mkdir -p etc app tmp
+
+          printf '%s\n' \
+            'root:x:0:0:root:/root:/sbin/nologin' \
+            'nonroot:x:65532:65532:nonroot:/nonexistent:/sbin/nologin' \
+            > etc/passwd
+
+          printf '%s\n' \
+            'root:x:0:' \
+            'nonroot:x:65532:' \
+            > etc/group
+
+          cp ${./app/seed.json} app/seed.json
+          chmod 1777 tmp
+        '';
+
+        created = "1970-01-01T00:00:01Z";
+
+        config = {
+          Entrypoint = [ "/bin/quicknotes" ];
+          ExposedPorts = {
+            "8080/tcp" = { };
+          };
+          User = "nonroot:nonroot";
+          WorkingDir = "/app";
+          Env = [
+            "ADDR=:8080"
+            "DATA_PATH=/tmp/quicknotes/notes.json"
+            "SEED_PATH=/app/seed.json"
+          ];
+        };
+      };
     in
     {
       packages.${system} = {
-        inherit quicknotes;
+        inherit quicknotes docker;
         default = quicknotes;
       };
 
