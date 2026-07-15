c9cf42d4d46595402d2d46ea9e7aae31c9a9a135
Abdra04 <a.ismailov@innopolis.university>
2026-07-15 19:10:17 +0300

    feat(lab11): add Nix flake and lockfile

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "QuickNotes — reproducible builds with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        go = pkgs.go_1_24;
+        buildGoModule = pkgs.buildGoModule.override { inherit go; };
+      in
+      {
+        packages.default = self.packages.${system}.quicknotes;
+
+        packages.quicknotes = buildGoModule {
+          pname = "quicknotes";
+          version = "0.1.0";
+          src = ./app;
+          vendorHash = null;   # проект не имеет внешних зависимостей
+          ldflags = [ "-s" "-w" ];
+          subPackages = [ "." ];
+          env.CGO_ENABLED = "0";
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_24
+            gopls
+            golangci-lint
+          ];
+        };
+      }
+    );
+}
